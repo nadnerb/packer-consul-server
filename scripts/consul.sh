c75e0d281aaff6343036ef4aed0ecf15d3557c8c
Brendan Spinks <brendanspinks@gmail.com>
2015-12-12 09:58:08 +1100

    updated paths and default to 0.6.0

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -5,7 +5,7 @@ set -e
 echo "Fetching Consul..."
 
 cd /tmp
-curl -L -o consul.zip https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_linux_amd64.zip
+curl -L -o consul.zip https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 
 echo "Installing Consul..."
 unzip consul.zip >/dev/null
@@ -16,14 +16,18 @@ sudo mkdir -p /mnt/consul
 sudo mkdir -p /etc/service
 
 echo "Fetching Consul template..."
-curl -L -o consul_template.zip https://github.com/hashicorp/consul-template/releases/download/v${CONSUL_TEMPLATE_VERSION}/consul_template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
+curl -L -o consul-template.zip https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
 
 echo "installing Consul template..."
-unzip consul_template.zip >/dev/null
+unzip consul-template.zip >/dev/null
 sudo mv consul-template /opt/consul-template
 
 # Install web ui
+echo "Fetching Consul UI"
+curl -L -o consul-ui.zip https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_web_ui.zip
+
 echo "Installing UI"
-wget https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_web_ui.zip -O consul-ui.zip
+sudo mkdir -p /mnt/consul/ui
+sudo mv consul-ui.zip /mnt/consul/ui
+cd /mnt/consul/ui
 unzip consul-ui.zip >/dev/null
-sudo mv dist /mnt/consul/ui
--- scripts/default.sh
+++ scripts/default.sh
@@ -3,6 +3,5 @@
 set -e
 
 # Updating and Upgrading dependencies
-apt-get update -y -qq > /dev/null
-apt-get upgrade -y -qq > /dev/null
-sudo apt-get install -y vim curl wget unzip screen
+yum -y update
+yum -y install vim curl unzip
